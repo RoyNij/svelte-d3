1f068bcd2fbbcb56a00b9efd21ca2a61bf7012e4
Roy Nijhof <roy@showntelldata.com>
2020-03-14 22:50:49 -0500

    Added button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,21 @@
+<button on:click>
+    <slot></slot>
+</button>
+
+<style>
+button{
+    appearance: none;
+    -webkit-appearance: none;
+    padding: 5px;
+    border-radius: 5px;
+    border: none;
+    background-color: #DEDEDE;
+    cursor: pointer;
+    line-height: 1;
+}
+
+button:focus{
+    outline: none;
+    background-color: aquamarine;
+}
+</style>
