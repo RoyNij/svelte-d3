7422dc6d16f53e5d4038b5c169f3ff3ecb0943bd
Roy Nijhof <roy@showntelldata.com>
2020-09-03 10:30:57 -0500

    Change the main svelte app to just show rotating globe for now

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,8 @@
 <script>
-    import ResponsiveWrapper from './components/ResponsiveWrapper.svelte';
-    import Map from './components/Map.svelte';
-    import Button from './components/Button.svelte';
+import ResponsiveWrapper from './components/ResponsiveWrapper.svelte';
+    import ResponsiveGlobe from './views/ResponsiveGlobe.svelte';
 </script>
 
 <div class='container'>
-    <ResponsiveWrapper let:width={width}>
-        <Map width={width} 
-            aspect="16:9" 
-            mapLocation='./data/colombia_mun.json'
-            mapCollection='colombia_mun'
-            pathId='COD'
-            showLabel={true}
-            labelVar='MUNICIPIO'
-        />
-    </ResponsiveWrapper>
+    <ResponsiveGlobe width={ 900 } />
 </div>
-
-
-
-<Map width={300} 
-    aspect="4:3" 
-    mapLocation='./data/colombia_mun.json'
-    mapCollection='colombia_mun'
-    pathId='COD'
-    showLabel={true}
-    labelVar='MUNICIPIO'
-/>
-<ResponsiveWrapper let:width={width}>
-    <Map width={Math.floor(width)} 
-        aspect="16:9" 
-        mapLocation='./data/colombia_mun.json'
-        mapCollection='colombia_mun'
-        pathId='COD'
-        showLabel={true}
-        labelVar='MUNICIPIO'
-    />
-</ResponsiveWrapper>
-
-<ResponsiveWrapper let:width={width}>
-    <Map width={Math.floor(width)} 
-        aspect="16:9" 
-        mapLocation='./data/world.topojson'
-        showLabel={true}
-        labelVar='name'
-    />
-</ResponsiveWrapper>
